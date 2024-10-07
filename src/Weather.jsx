de43e61bc2fb1ac56f342b5f237e6780872b68ed
Soonyoung Park <soon.syp@gmail.com>
2024-10-08 00:38:39 +0200

    weather init

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import ReactDOM from 'react-dom/client'
+import { GLOBAL } from './Global'
 import './index.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faXmark} from '@fortawesome/free-solid-svg-icons'
@@ -17,6 +17,29 @@ export default function Weather(){
   const head = "Weather"
   const [active, setActive] = useState(false);
 
+  const d = new Date()
+  let month = d.getMonth()
+  let date = d.getDate()
+  let day = d.getDay()
+  let hour = d.getHours();
+  let minute = d.getMinutes(); 
+
+  const months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+  const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
 
   return(
     <div className='bg'>
@@ -29,15 +52,56 @@ export default function Weather(){
       </Link>
 
       <div className='content'>
+        <div className="center info" style={{
+          color:'white',
+          margin: '0 0 1rem 0',
+        }}>
+        <p>{days[day]} {date} {months[month]} </p>
+        <p>HAMBURG</p>
+        </div>
+
+        <div className='clock'>
+          <div className='tick bigTick Uhr12'></div>
+          <div className='tick bigTick rotate Uhr3'></div>
+          <div className='tick bigTick Uhr6'></div>
+          <div className='tick bigTick rotate Uhr9'></div>
 
 
-      <div className='margin-bottom center'>
+
+
+        </div>
+
+        <table className='weatherInfo'>
+          <tr>
+            <th>Change of Rain</th>
+            <td>0%</td>
+          </tr>
+          <tr>
+            <th>Humitidy</th>
+            <td>35%</td>
+          </tr>
+          <tr>
+            <th>PM 10</th>
+            <td>81</td>
+          </tr>
+          <tr>
+            <th>UV Index</th>
+            <td>0</td>
+          </tr>
+        </table>
+
+        <div className='weekWeather'>
+          
+        </div>
+
+
+
 
       </div>
 
 
        
-      </div>
+
     </div>
   )
 }
