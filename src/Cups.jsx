ba681a0cec29a6307df1050ac6249f881c55f2a3
Soonyoung Park <soon.syp@gmail.com>
2024-10-14 23:18:35 +0200

    update cup pg

--- src/Cups.jsx
+++ src/Cups.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef, createRef } from 'react'
 import { animated, useSpring } from '@react-spring/web'
+import { GLOBAL } from './Global'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -7,14 +8,43 @@ import { faXmark} from '@fortawesome/free-solid-svg-icons'
 
 import { Link } from 'react-router-dom'
 
-export default function Weather(){
+export default function Cups(){
   
   const head = "Cups"
   const [cupNum, setCup] = useState(1);
+  const [cupAmount, setAmount] = useState(350);
   const slideNum = [25, -25, -75, -125]
   const slideCup = useSpring({
     transform: `translateX(${slideNum[cupNum]}vw)`
   })
+  let cupAmountScroll = createRef()
+  let startNum = 0
+  let num = startNum
+  let step = 10
+  let endNum = 2000 - step
+  var totalNum = ((endNum - startNum)/step + 10);
+  let cupArray = []
+  let width = window.innerWidth
+  let ratio = width/window.innerHeight
+  let colWidth = ratio >= 1? width / 100 * 11.11 : width / 100 * 20
+
+  while (num < endNum + step + 1) {
+    cupArray.push(num)
+    num = num + step
+  }
+
+  function getFianlNum() {
+    width = window.innerWidth
+    ratio = width/window.innerHeight
+    colWidth = ratio >= 1? width / 100 * 11.11 : width / 100 * 20
+    const finalValue = Math.round(((cupAmountScroll.current.scrollLeft)/colWidth)*step);
+    setAmount(finalValue)
+    console.log(cupAmount)
+  }
+
+  useEffect(()=> {
+    cupAmountScroll.current.scrollLeft = cupAmount / step * colWidth
+  }, [cupAmountScroll, cupAmount, cupNum])
 
   return(
 
@@ -34,19 +64,66 @@ export default function Weather(){
         <img className={cupNum == 3? 'selected' : 'notSelected'} src='/cups/3.png' onMouseUp={() => setCup(3)} onClick={() => setCup(3)} />
       </animated.div>
 
-      <div className='cupAmount'>
-      <div id="scroller">
- 		    <div id="scroller-block"></div>
-	    </div>
+      <div className='cupAmountBg' style={{
+        position:'relative',
+        top: '3rem',
+        width: '90vw',
+        height: '3rem',
+        margin: 'auto',
+        backgroundImage: 'linear-gradient(to right, rgba(255,255,255,0), rgba(255,255,255,0.4), rgba(255,255,255,0))',
+      }}>
+        <div className='cupSelectionBox' style={{
+          position:'relative',
+          height: '3rem',
+          background: GLOBAL.backgroundDunkeler,
+          left: '50%',
+          transform: 'translateX(-50%)',
+          borderRadius: '0.6rem'
+        }}>
+        </div>
+        <div style={{
+          position:'relative',
+          width: 0,
+          height: 0,
+          borderLeft: '0.8rem solid transparent',
+          borderRight: '0.8rem solid transparent',
+          borderTop: '1rem solid ' + GLOBAL.backgroundDunkeler,
+          margin: 'auto',
+        }}>
+        </div>
+      </div>
+
+      <div ref= { cupAmountScroll } className='cupAmount' style={{
+        position:'relative',
+        top: '0rem',
+        width: '100vw',
+        height: '3rem',
+        margin: 'auto',
+        background: 'transparent',
+        overflowX: 'auto',
+        overflowY: 'hidden',
+        scrollSnapType: 'x mandatory',
+        scrollSnapStop: 'always',
+        display: 'grid',
+      }}>
+        {cupArray.map((el, index) => (
+          <p key={ index } className='cupArray' style={{
+            position: 'relative',
+            color: 'white',
+            margin: 'auto',
+            fontWeight: 600,
+            fontSize: '1.1rem',
+            textAlign: 'center',
+            scrollSnapAlign: 'center',
+          }}>{el}ml</p>
+        ))}
       </div>
 
       <div className='btn center' style={{
-        top: '7rem',
-        position: 'relative'
         }}>
-          <Link to='/'>
-            <button>USE</button>
-          </Link>
+
+            <button onClick={ getFianlNum }>USE</button>
+
       </div>
     
     </div>
