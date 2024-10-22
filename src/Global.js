66f8f27939d77c466277e7eb7d86169e2427c565
Soonyoung Park <soon.syp@gmail.com>
2024-10-23 01:09:05 +0200

    add data

--- src/Global.js
+++ src/Global.js
@@ -23,9 +23,47 @@ const GLOBAL = {
     'Dec',
   ],
   days : ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+
+  weight: 70,
+  age: 2000,
+  duration: 60,
+  activity: false,
   todaysGoal: 2100,
+
   cupNum: 1,
   cupAmount: [190,350,600,1000],
+
+  tabNum: 0,
+  notification: false,
+  repeatMon: false,
+  repeatTue: false,
+  repeatWed: false,
+  repeatThu: false,
+  repeatFri: false,
+  repeatSat: false,
+  repeatSun: false,
+  interval: 60,
+  timeRangeStart: 9,
+  timeRangeEnd: 22,
+
+  filterMon: false,
+  filterTue: false,
+  filterWed: false,
+  filterThu: false,
+  filterFri: false,
+  filterSat: false,
+  filterSun: false,
+
+
+  alarmData: [
+    { id: 0, hour: 10, min: 0, tags: ['Mon']},
+    { id: 1, hour: 9, min: 10, tags: ['Tue', 'Wed']},
+    { id: 2, hour: 13, min: 20, tags: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri']},
+    { id: 3, hour: 14, min: 30, tags: ['Mon', 'Tue']},
+
+  ],
+
+
 }
 
 export { GLOBAL }
