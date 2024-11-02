34b04e75335c90eb87ef636f5b29586dfd33805b
Soonyoung Park <soon.syp@gmail.com>
2024-11-02 15:34:50 +0100

    update sunmane

--- src/component_weather/Sun.jsx
+++ src/component_weather/Sun.jsx
@@ -4,7 +4,6 @@ export default function Sun({daytime}){
     <>
       <img src="/weather/sunMane.png" alt="sun" className="sunMane" style={{
         position: "absolute",
-        transform: 'translate(-50%, -50%)',
         top: '50%',
         left: '50%',
         height: '100%',
