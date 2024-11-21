0f9652948b0070ec94806147df8b3a8636df892e
Soonyoung Park <soon.syp@gmail.com>
2024-11-21 22:11:59 +0100

    revert table

--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -483,54 +483,6 @@ export default function Weather(){
             <th>PM2.5</th>
             <td>{airQuality25(Math.round(airData.european_aqi_pm2_5[index]))}</td>
           </tr>
-
-          <tr>
-            <th>Min/Max Temp</th> 
-            <td> 
-              <span style={{ color: GLOBAL.backgroundHell}}>{ dailyData.apparent_temperature_min[0] }° </span>
-              <span>{ dailyData.apparent_temperature_max[0] }°</span>         
-              </td>
-          </tr>
-          <tr>
-            <th>UV Index</th> 
-            <td>{ uvIndex(hourlyData.uv_index[index]) }</td>
-          </tr>
-          <tr>
-            <th>Chance of Rain</th>
-            <td>{ hourlyData.precipitation_probability[index] }%</td>
-          </tr>
-          <tr>
-            <th>PM 10</th>
-            <td>{airQuality10(Math.round(airData.european_aqi_pm10[index]))}</td>
-          </tr>
-          <tr>
-            <th>PM2.5</th>
-            <td>{airQuality25(Math.round(airData.european_aqi_pm2_5[index]))}</td>
-          </tr>
-
-          <tr>
-            <th>Min/Max Temp</th> 
-            <td> 
-              <span style={{ color: GLOBAL.backgroundHell}}>{ dailyData.apparent_temperature_min[0] }° </span>
-              <span>{ dailyData.apparent_temperature_max[0] }°</span>         
-              </td>
-          </tr>
-          <tr>
-            <th>UV Index</th> 
-            <td>{ uvIndex(hourlyData.uv_index[index]) }</td>
-          </tr>
-          <tr>
-            <th>Chance of Rain</th>
-            <td>{ hourlyData.precipitation_probability[index] }%</td>
-          </tr>
-          <tr>
-            <th>PM 10</th>
-            <td>{airQuality10(Math.round(airData.european_aqi_pm10[index]))}</td>
-          </tr>
-          <tr>
-            <th>PM2.5</th>
-            <td>{airQuality25(Math.round(airData.european_aqi_pm2_5[index]))}</td>
-          </tr>
           </tbody>
         </table>)}
 
